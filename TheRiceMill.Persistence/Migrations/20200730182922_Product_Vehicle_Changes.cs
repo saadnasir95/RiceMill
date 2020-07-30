63482d4bffb1ce0a1d35f6a8a23961fff871a6fc
saadnasir95 <saadnasir95@gmail.com>
2020-07-30 23:33:00 +0500

    DB changes for vehicle and product

---
+++ TheRiceMill.Persistence/Migrations/20200730182922_Product_Vehicle_Changes.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TheRiceMill.Persistence.Migrations
+{
+    public partial class Product_Vehicle_Changes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "NormalizedName",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Products");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Vehicles",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "NormalizedName",
+                table: "Vehicles",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Price",
+                table: "Products",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Products",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
